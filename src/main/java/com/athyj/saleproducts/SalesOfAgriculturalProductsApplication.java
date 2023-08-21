cfdf12f63d08d9bbdbdb28de3355bf7ea0b6c45b
Outlier <Outlier@atchina.com>
2023-08-21 10:24:05 +0800

    第一次

---
+++ src/main/java/com/athyj/saleproducts/SalesOfAgriculturalProductsApplication.java
@@ -0,0 +1,13 @@
+package com.athyj.saleproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesOfAgriculturalProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesOfAgriculturalProductsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/athyj/saleproducts/SalesOfAgriculturalProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athyj.saleproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesOfAgriculturalProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
